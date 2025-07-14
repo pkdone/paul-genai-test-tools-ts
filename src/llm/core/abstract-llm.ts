44d16ab31a2b09b683410c23293a6bb2116cc2de
Paul Done <pkdone@gmail.com>
2025-07-14 10:39:53 +0100

    refactorings

--- src/llm/core/abstract-llm.ts
+++ src/llm/core/abstract-llm.ts
@@ -170,61 +170,6 @@ export default abstract class AbstractLLM implements LLMProviderImpl {
   }
 
   /**
-   * Extract token usage information from LLM response metadata, defaulting missing
-   * values.
-   */
-  private extractTokensAmountFromMetadataDefaultingMissingValues(
-    modelKey: string,
-    tokenUsage: LLMResponseTokensUsage,
-    modelsMetadata: Record<string, ResolvedLLMModelMetadata>,
-  ): LLMResponseTokensUsage {
-    let { promptTokens, completionTokens, maxTotalTokens } = tokenUsage;
-    if (completionTokens < 0) completionTokens = 0;
-    if (maxTotalTokens < 0) maxTotalTokens = modelsMetadata[modelKey].maxTotalTokens;
-    if (promptTokens < 0) promptTokens = Math.max(1, maxTotalTokens - completionTokens + 1);
-    return { promptTokens, completionTokens, maxTotalTokens };
-  }
-
-  /**
-   * Post-process the LLM response, converting it to JSON if necessary, and build the
-   * response metadaat object.
-   */
-  private postProcessAsJSONIfNeededGeneratingNewResult(
-    skeletonResult: LLMFunctionResponse,
-    modelKey: string,
-    taskType: LLMPurpose,
-    responseContent: LLMGeneratedContent,
-    completionOptions: LLMCompletionOptions | undefined,
-    context: LLMContext,
-    modelsMetadata: Record<string, ResolvedLLMModelMetadata>,
-    logProcessingWarning = false,
-  ): LLMFunctionResponse {
-    if (taskType === LLMPurpose.COMPLETIONS) {
-      try {
-        const generatedContent =
-          completionOptions?.outputFormat === LLMOutputFormat.JSON
-            ? convertTextToJSONAndOptionallyValidate(responseContent, completionOptions)
-            : responseContent;
-        return {
-          ...skeletonResult,
-          status: LLMResponseStatus.COMPLETED,
-          generated: generatedContent,
-        };
-      } catch (error: unknown) {
-        if (logProcessingWarning) {
-          console.warn(
-            `LLM response for model '${modelsMetadata[modelKey].urn}' cannot be parsed to JSON so marking as overloaded just to be able to try again in the hope of a better response for the next attempt - Error: ${getErrorText(error)}`,
-          );
-        }
-        context.jsonParseError = getErrorText(error);
-        return { ...skeletonResult, status: LLMResponseStatus.OVERLOADED };
-      }
-    } else {
-      return { ...skeletonResult, status: LLMResponseStatus.COMPLETED, generated: responseContent };
-    }
-  }
-
-  /**
    * Executes the LLM function for the given model key and task type.
    */
   private async executeLLMImplFunction(
@@ -284,6 +229,61 @@ export default abstract class AbstractLLM implements LLMProviderImpl {
       } else {
         return { ...skeletonResponse, status: LLMResponseStatus.ERRORED, error };
       }
+    }
+  }
+
+  /**
+   * Extract token usage information from LLM response metadata, defaulting missing
+   * values.
+   */
+  private extractTokensAmountFromMetadataDefaultingMissingValues(
+    modelKey: string,
+    tokenUsage: LLMResponseTokensUsage,
+    modelsMetadata: Record<string, ResolvedLLMModelMetadata>,
+  ): LLMResponseTokensUsage {
+    let { promptTokens, completionTokens, maxTotalTokens } = tokenUsage;
+    if (completionTokens < 0) completionTokens = 0;
+    if (maxTotalTokens < 0) maxTotalTokens = modelsMetadata[modelKey].maxTotalTokens;
+    if (promptTokens < 0) promptTokens = Math.max(1, maxTotalTokens - completionTokens + 1);
+    return { promptTokens, completionTokens, maxTotalTokens };
+  }
+
+  /**
+   * Post-process the LLM response, converting it to JSON if necessary, and build the
+   * response metadaat object.
+   */
+  private postProcessAsJSONIfNeededGeneratingNewResult(
+    skeletonResult: LLMFunctionResponse,
+    modelKey: string,
+    taskType: LLMPurpose,
+    responseContent: LLMGeneratedContent,
+    completionOptions: LLMCompletionOptions | undefined,
+    context: LLMContext,
+    modelsMetadata: Record<string, ResolvedLLMModelMetadata>,
+    logProcessingWarning = false,
+  ): LLMFunctionResponse {
+    if (taskType === LLMPurpose.COMPLETIONS) {
+      try {
+        const generatedContent =
+          completionOptions?.outputFormat === LLMOutputFormat.JSON
+            ? convertTextToJSONAndOptionallyValidate(responseContent, completionOptions)
+            : responseContent;
+        return {
+          ...skeletonResult,
+          status: LLMResponseStatus.COMPLETED,
+          generated: generatedContent,
+        };
+      } catch (error: unknown) {
+        if (logProcessingWarning) {
+          console.warn(
+            `LLM response for model '${modelsMetadata[modelKey].urn}' cannot be parsed to JSON so marking as overloaded just to be able to try again in the hope of a better response for the next attempt - Error: ${getErrorText(error)}`,
+          );
+        }
+        context.jsonParseError = getErrorText(error);
+        return { ...skeletonResult, status: LLMResponseStatus.OVERLOADED };
+      }
+    } else {
+      return { ...skeletonResult, status: LLMResponseStatus.COMPLETED, generated: responseContent };
     }
   }
 
